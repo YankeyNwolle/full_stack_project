9aade1bd2c2c0a07ca9127305297b1540d87f7f9
yankey <nwolle14@gmail.com>
2026-02-06 01:37:26 +0000

    connexion express a postgresql

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+// connexion à la base de données
+import pkg from "pg";
+const { Pool } = pkg;
+
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,  
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  port: process.env.PORTDB,
+});
+
+pool.connect("connect"), () => {
+    console.log("connexion à la base de données réussie");
+}  
+
+export default pool;
