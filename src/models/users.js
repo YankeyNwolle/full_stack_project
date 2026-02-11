becae8433ed75f35b96fb474d9fd98463cb6896a
yankey <nwolle14@gmail.com>
2026-02-11 11:51:39 +0000

    Models utilisateurs

--- src/models/users.js
+++ src/models/users.js
@@ -1,14 +1,27 @@
-import express from 'express';
-const router = express.Router(); 
+import pool from '../config/database.js';
 
-// GET sur /api/users/
-router.get('/', (req, res) => {
-    res.send('Liste des utilisateurs');
-});
+export const GetAllUsersblog = async () => {
+    const result = await pool.query('SELECT * FROM users');
+    return result.rows;
+}
 
-// POST sur /api/users/
-router.post('/', (req, res) => {
-    res.send('Créer un utilisateur');
-});
+export const GetUsersByIdblog = async (id) => {
+    const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    return result.rows[0];
+}
 
-export default router; 
+export const UpdateUsersblog = async (id, username, email) => {
+    const result = await pool.query('UPDATE users SET id = $1, username = $2 WHERE email = $3 RETURNING *', [id, username, email]);
+    return result.rows[0];
+}
+
+export const DeleteUsersblog = async (id) => {
+    const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+    return result.rows[0];
+}
+
+export const CreateUsersblog = async (username, password, email) => {
+    const result = await pool.query('INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *', [username, password, email]);
+    return result.rows[0];
+}
+
