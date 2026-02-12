6b49182b66976f17af2144ca8b3087a2014f831d
yankey <nwolle14@gmail.com>
2026-02-12 01:21:15 +0000

    Créer un models de commentaire avec la methode CRUD

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,15 +1,33 @@
-// routes/post.js
-const express = require('express');
-const router = express.Router(); // 1. Création du routeur
+import pool from '../config/database.js';
 
-// Définition d'une route GET sur /api/comments/
-router.get('/', (req, res) => {
-    res.send('Liste des commentaires');
-});
 
-// Définition d'une route POST sur /api/comments/
-router.post('/', (req, res) => {
-    res.send('Créer un commentaire');
-});
+// recupère tous les commentaires des utilisateur de la base de données
+export const GetAllCommentblog = async () => {
+    const result = await pool.query('SELECT * FROM comments');
+    return result.rows;
+}
 
-module.exports = router; // 2. Exportation du routeur
+// recupère un commentaire par son id
+export const GetCommentByIdblog = async (id) => {
+    const result = await pool.query('SELECT * FROM comments WHERE id = $1', [id]);
+    return result.rows[0];
+}
+
+// met à jour un commentaire par son id
+export const UpdateCommentblog = async (id, content) => {
+    const result = await pool.query('UPDATE comments SET content = $1 WHERE id = $2 RETURNING *', [content, id]);
+    return result.rows[0];
+}
+
+// supprime un commentaire par son id
+export const DeleteCommentblog = async (id) => {
+    const result = await pool.query('DELETE FROM comments WHERE id = $1 RETURNING *', [id]);
+    return result.rows[0];
+}
+
+// crée un nouveau commentaire
+export const CreateCommentblog = async (content) => {
+    const result = await pool.query('INSERT INTO comments (content) VALUES ($1) RETURNING *', [content]);
+    return result.rows[0];
+}
+ 
