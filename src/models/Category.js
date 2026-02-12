f3261247f858e98676f22608607183d3a9c6a483
yankey <nwolle14@gmail.com>
2026-02-12 01:21:54 +0000

    Créer un models de categorie avec la methode CRUD

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,16 +1,33 @@
-// routes/post.js
-const express = require('express');
-const router = express.Router(); 
+import pool from '../config/database.js';
 
-//  GET  /api/topic/
-router.get('/', (req, res) => {
-    res.send('Liste des sujets');
-});
+// recupère tous les categories des utilisateurs de la base de données
+export const GetAllCategoryblog = async () => {
+    const result = await pool.query('SELECT * FROM categories');
+    return result.rows;
+}
 
-// POST  /api/topic/
-router.post('/', (req, res) => {
-    res.send('Créer un sujet');
-});
+// recupère une catégorie par son id
+export const GetCategoryByIdblog = async (id) => {
+    const result = await pool.query('SELECT * FROM categories WHERE id = $1', [id]);
+    return result.rows[0];
+}
 
-// exportation du routeur
-module.exports = router; 
+// met à jour une catégorie par son id
+export const UpdateCategoryblog = async (id, name) => {
+    const result = await pool.query('UPDATE categories SET name = $1 WHERE id = $2 RETURNING *', [name, id]);
+    return result.rows[0];
+}
+
+// supprime une catégorie par son id
+export const DeleteCategoryblog = async (id) => {
+    const result = await pool.query('DELETE FROM categories WHERE id = $1 RETURNING *', [id]);
+    return result.rows[0];
+}
+
+// crée une nouvelle catégorie
+export const CreateCategoryblog = async (name) => {
+    const result = await pool.query('INSERT INTO categories (name) VALUES ($1) RETURNING *', [name]);
+    return result.rows[0];
+}
+ 
+
