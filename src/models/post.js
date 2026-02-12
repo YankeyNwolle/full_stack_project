c21229c8fc8b2a794965bd3f66d56083a3be0756
yankey <nwolle14@gmail.com>
2026-02-12 02:53:53 +0000

    Créer un models de post avec la methode CRUD

--- src/models/post.js
+++ src/models/post.js
@@ -12,15 +12,19 @@ export const GetPostByIdblog = async (id) => {
     return result.rows[0]
 }
 
-
+// met à jour un post par son id
 export const UpdatePostblog = async (id, title, image, content) => {
     const result = await pool.query('UPDATE posts SET title = $1, image = $2, content = $3 WHERE id = $4 RETURNING *', [title, image, content, id]);
     return result.rows[0];
 }
+
+// supprime un post par son id
 export const DeletePostblog = async (id) => {
     const result = await pool.query('DELETE FROM posts WHERE id = $1 RETURNING *', [id]);
     return result.rows[0];
 }
+
+// crée un nouveau post
 export const CreatePostblog = async (title, image, content) => {
     const result = await pool.query('INSERT INTO posts (title, image, content) VALUES ($1, $2, $3) RETURNING *', [title, image, content]);
     return result.rows[0];
