77151243645a456150c77bab84e20278502ef6a5
yankey <nwolle14@gmail.com>
2026-02-12 01:20:34 +0000

    Créer un models de post avec la méthode CRUD

--- src/models/post.js
+++ src/models/post.js
@@ -1,15 +1,28 @@
-// routes/post.js
-const express = require('express');
-const router = express.Router(); 
+import pool from '../config/database.js';
 
-//  GET sur /api/post/
-router.get('/', (req, res) => {
-    res.send('Liste des articles');
-});
+// recupère tous les post des utilisateurs de la base de données
+export const GetAllUPostblog = async () => {
+   const result = await pool.query('SELECT * FROM posts');
+   return result.rows;
+}
 
-//  POST sur /api/post/
-router.post('/', (req, res) => {
-    res.send('Créer un article');
-});
+// recupère un par son id
+export const GetPostByIdblog = async (id) => {
+    const result = await pool.query('SELECT * FROM posts WHERE id = $1', [id]);
+    return result.rows[0]
+}
 
-module.exports = router;
+
+export const UpdatePostblog = async (id, title, image, content) => {
+    const result = await pool.query('UPDATE posts SET title = $1, image = $2, content = $3 WHERE id = $4 RETURNING *', [title, image, content, id]);
+    return result.rows[0];
+}
+export const DeletePostblog = async (id) => {
+    const result = await pool.query('DELETE FROM posts WHERE id = $1 RETURNING *', [id]);
+    return result.rows[0];
+}
+export const CreatePostblog = async (title, image, content) => {
+    const result = await pool.query('INSERT INTO posts (title, image, content) VALUES ($1, $2, $3) RETURNING *', [title, image, content]);
+    return result.rows[0];
+}
+ 
