304029410f63e856433fc037961198829b9f122a
yankey <nwolle14@gmail.com>
2026-03-04 10:50:12 +0000

    Checkpoint from VS Code for cloud agent session

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: 'votre_secret_ici',
+  secret: 'TOPSECRET',
   resave: false,
   saveUninitialized: false
 }));
